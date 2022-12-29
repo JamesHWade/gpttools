1c04fe90c320352147d9a2009384806aa549d4f4
James Wade <github@jameshwade.com>
2022-12-28 20:28:41 -0500

    edit readme

--- R/suggest_test_addin.R
+++ R/suggest_test_addin.R
@@ -6,7 +6,7 @@
 suggest_unit_test_addin <- function() {
   gpt_insert(
     model = "text-davinci-003",
-    prompt = "Suggest a unit text for this function use the testthat package",
+    prompt = "Suggest a unit text for this function using the testthat package",
     temperature = 0.5,
     max_tokens = 1000,
     append_text = TRUE
