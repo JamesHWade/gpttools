86efce66fc2eb0e876bc0d62e70cf228af906753
James Wade <jhwade@dow.com>
2023-01-06 11:07:43 -0500

    tidy upkeep work

--- R/gpttools-package.R
+++ R/gpttools-package.R
@@ -1,6 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @import rlang
 #' @import cli
 #' @importFrom assertthat assert_that is.string is.number is.count is.flag
 #' @importFrom glue glue
 #' @importFrom utils globalVariables
+## usethis namespace: end
 NULL
