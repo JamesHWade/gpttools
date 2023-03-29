577be678b889044561f26dda682db1448db2a71d
James Wade <jhwade@dow.com>
2023-03-29 17:02:37 -0400

    a bit stuck on history

--- R/history.R
+++ R/history.R
@@ -15,95 +15,93 @@ create_history <- function(file_name = "chat_history",
       timestamp = lubridate::now(),
       role = character(),
       content = character(),
+      hash = character()
     )
     arrow::write_parquet(query_history, file_path)
   }
 }
-
 
 read_history <- function(file_name = "chat_history") {
   file_path <- get_history_path(file_name)
   arrow::read_parquet(file_path)
 }
 
-
 save_history <- function(file_name = "chat_history",
-                         history = read_history(),
                          role,
-                         content) {
-  file_path <- get_history_path(file_name)
+                         content,
+                         overwrite = FALSE) {
+  create_history(file_name, overwrite = overwrite)
+  history <- read_history(file_name)
+  embedding <- create_openai_embedding(content)
 
   new_entry <- tibble::tibble(
-    id = as.integer(0), # dummy value, will be replaced
+    id        = max(0, history$id, na.rm = TRUE) + 1,
     timestamp = lubridate::now(),
-    role = role,
-    content = content
+    role      = role,
+    content   = content,
+    embedding = embedding$embedding,
+    hash      = hash_md5(content)
   )
 
-  new_entry$id <- max(0, history$id, na.rm = TRUE) + 1
-  query_history <- history |> dplyr::bind_rows(new_entry)
-
-  arrow::write_parquet(query_history, file_path)
+  new_entry |>
+    rbind(history) |>
+    arrow::write_parquet(get_history_path(file_name))
 }
 
+get_query_embedding <- function(query) {
+  create_openai_embedding(input_text = query) |>
+    dplyr::pull(embedding) |>
+    unlist()
+}
 
-create_history_embeddings <- function(file_name = "chat_embeddings",
-                                      overwrite = FALSE) {
-  file_path <- get_history_path(file_name)
+get_query_context <- function(query_embedding, full_context, k, column_name = "chunks") {
+  check_context(full_context)
+  get_top_matches(full_context, query_embedding, k = k) |>
+    dplyr::pull({{ column_name }}) |>
+    paste(collapse = "\n\n")
+}
 
-  if (!file.exists(file_path) || overwrite) {
-    query_history_embeddings <- tibble(
-      id = integer(),
-      embedding = list()
+check_context <- function(context) {
+  if (rlang::is_null(context)) {
+    cli_abort(
+      "You specified that context should be added but none was provided."
     )
-
-    arrow::write_parquet(query_history_embeddings, file_path)
+  } else if (!is.data.frame(context)) {
+    cli_abort(
+      "You passed a {class(context)} to but a data.frame was expected."
+    )
   }
 }
 
-
-save_history_and_embeddings <- function(file_name = "chat_history",
-                                        role,
-                                        content,
-                                        history = read_query_history(),
-                                        overwrite = FALSE) {
-  create_history()
-  create_history_embeddings()
-  save_history(file_name, history, role, content)
-  embedding <- create_openai_embedding(query)
-  file_path <- get_history_path("query_history_embeddings")
-  query_history_embeddings <- arrow::read_parquet(file_path)
-
-  new_embedding <- tibble(
-    id = max(0, query_history_embeddings$id, na.rm = TRUE) + 1,
-    embedding = list(embedding$embedding)
-  )
-
-  query_history_embeddings <-
-    query_history_embeddings |> dplyr::bind_rows(new_embedding)
-  arrow::write_parquet(query_history_embeddings, file_path)
-}
-
-
-custom_chat_function <- function(index,
-                                 query,
-                                 history,
+custom_chat_function <- function(query,
+                                 index = NULL,
+                                 add_context = TRUE,
+                                 chat_history = NULL,
+                                 session_history = NULL,
+                                 add_history = TRUE,
                                  task = "Context Only",
                                  k = 4,
                                  save_history = TRUE,
                                  overwrite = FALSE) {
   arg_match(task, c("Context Only", "Permissive Chat"))
 
-  query_embedding <- create_openai_embedding(input_text = query) |>
-    dplyr::pull(embedding) |>
-    unlist()
+  if (rlang::is_true(add_context) || rlang::is_true(add_history)) {
+    query_embedding <- get_query_embedding(query)
+  }
 
-  full_context <- get_top_matches(index, query_embedding, k = k)
+  if (rlang::is_true(add_context)) {
+    context <-
+      get_query_context(query_embedding, index, k, column_name = "chunks")
+  }
 
-  context <-
-    full_context |>
-    dplyr::pull(chunks) |>
-    paste(collapse = "\n\n")
+  cli::cli_inform("up here")
+
+  if (rlang::is_true(add_history)) {
+    cli::cli_inform("some history")
+    related_history <-
+      get_query_context(query_embedding, history, k, column_name = "content")
+    cli::cli_inform("not the problem")
+  }
 
   instructions <-
     switch(task,
@@ -115,7 +113,7 @@ custom_chat_function <- function(index,
               glue(
                 "You are a helpful chat bot that answers questions based
                      on the context provided by the user. If the user does not
-                     provide context, say \"I am not able to answer that
+                     provide related context, say \"I am not able to answer that
                      question. Maybe try rephrasing your question in a
                      different way.\"\n\n---\n\nContext:\n\n{context}\n\n---"
               )
@@ -146,22 +144,22 @@ custom_chat_function <- function(index,
         )
     )
 
-  history <-
-    purrr::map(history, \(x) if (x$role == "system") NULL else x) |>
-    purrr::compact()
+  cli_inform("here")
+  # session_history <-
+  #   purrr::map(session_history, \(x) if (x$role == "system") NULL else x) |>
+  #   purrr::compact()
 
-  prompt <- c(history, instructions)
+  prompt <- c(session_history, instructions)
 
+  cli_inform("overhere")
   answer <- gptstudio::openai_create_chat_completion(prompt)
 
-  history <- read_query_history(file_name)
-
+  cli_inform("made it")
   if (rlang::is_true(save_history)) {
     save_history_and_embeddings(
       file_name = file_name,
       role      = "system",
       content   = instructions[[1]]$content,
-      history   = history,
       overwrite = overwrite
     )
 
@@ -169,7 +167,6 @@ custom_chat_function <- function(index,
       file_name = file_name,
       role      = "user",
       content   = query,
-      history   = history,
       overwrite = overwrite
     )
 
@@ -177,7 +174,6 @@ custom_chat_function <- function(index,
       file_name = file_name,
       role      = "assistant",
       content   = answer$response["choices"][[1]]$text,
-      history   = history,
       overwrite = overwrite
     )
   }
---
+++ tests/testthat/test-save_history.R
@@ -0,0 +1,6 @@
+# test_that("multiplication works", {
+#   skip_if_offline()
+#   save_history_and_embeddings(role = "user", content = "this is a test")
+#   save_history_and_embeddings(role = "user", content = "this is a test",
+#                               overwrite = TRUE)
+# })
