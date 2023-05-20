8987ab5790c4c9210948be2b8e4c9f75d3c38d1a
James Wade <github@jameshwade.com>
2023-05-19 21:16:25 -0400

    a start at local embeddings

---
+++ R/py-embedding.R
@@ -0,0 +1,13 @@
+get_transformer_model <- function(model_name = 'all-mpnet-base-v2') {
+  py_pkg_is_available()
+  transformer <- reticulate::import("sentence_transformers")
+  cli::cli_process_start("Downloading model. This may take a few minutes.")
+  model <- transformer$SentenceTransformer(model_name)
+  cli::cli_process_done()
+  model
+}
+
+create_text_embeddings <- function(text) {
+  model <- get_transformer_model()
+  purrr::map(text, \(x) model$encode(x) |> as.numeric())
+}
