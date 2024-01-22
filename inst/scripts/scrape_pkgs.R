7419cc9790e819544b024a649310c4faba145d8c
James Wade <github@jameshwade.com>
2024-01-22 16:14:32 -0500

    better index creation

--- inst/scripts/scrape_pkgs.R
+++
@@ -1,5 +0,0 @@
-library(gpttools)
-cli::cli_inform("Scraping package sites and creating an index for each.")
-options(repos = c(CRAN = "https://packagemanager.posit.co/all/latest"))
-options("repos")
-scrape_pkg_sites()
