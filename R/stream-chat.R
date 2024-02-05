ce99fe533b2eff3c2091a063c16d6041ef1c8801
James Wade <github@jameshwade.com>
2024-02-05 06:58:19 -0500

    feat: streaming function with parser

---
+++ R/stream-chat.R
@@ -0,0 +1,102 @@
+stream_chat_shiny <- function(prompt, service, r, output_id) {
+  switch(service,
+    "openai" = {
+      response <- stream_chat_openai(
+        prompt = prompt,
+        element_callback = create_handler_for_shiny("openai", r, output_id)
+      )
+    },
+    "anthropic" = {
+      response <- stream_chat_anthropic(
+        prompt = prompt,
+        element_callback = create_handler_for_shiny("anthropic", r, output_id)
+      )
+    },
+    "perplexity" = {
+      response <- stream_chat_perplexity(
+        prompt = prompt,
+        element_callback = create_handler_for_shiny("perplexity", r, output_id)
+      )
+    },
+    "cohere" = {
+      response <- stream_chat_cohere(
+        prompt = prompt,
+        element_callback = create_handler_for_shiny("cohere", r, output_id)
+      )
+    },
+    "ollama" = {
+      response <- stream_chat_ollama(
+        prompt = prompt,
+        element_callback = create_handler_for_shiny("ollama", r, output_id)
+      )
+    }
+  )
+}
+
+create_handler_for_shiny <- function(service = "openai", r, output_id = "streaming") {
+  env <- rlang::env()
+  env$resp <- NULL
+  env$full_resp <- NULL
+
+  stream_details <- get_stream_pattern(service)
+  new_pattern <- stream_details$pattern
+  new_pluck <- stream_details$pluck
+
+  # Use quosures to capture the expressions
+  pattern_quo <- rlang::quo(new_pattern)
+
+  mask <- new_data_mask(env)
+
+  # Define the handler function with injected values
+  rlang::eval_tidy(rlang::expr(function(x) {
+    x <- rawToChar(x)
+    pattern <- !!pattern_quo
+    if (rlang::is_null(env$resp)) {
+      env$resp <- x
+    } else {
+      env$resp <- paste0(env$resp, x)
+    }
+
+    if (stringr::str_detect(env$resp, pattern)) {
+      parsed <- stringr::str_extract(env$resp, pattern) %>%
+        jsonlite::fromJSON() %>%
+        purrr::pluck(!!!new_pluck)
+      env$full_resp <- paste0(env$full_resp, parsed)
+      shinyjs::html(
+        output_id,
+        shiny::markdown(paste("**Assistant**", env$full_resp, sep = "\n\n"))
+      )
+      r$response <- env$full_resp
+      env$resp <- stringr::str_split(env$resp, pattern)
+      env$resp <- env$resp[[1]][[length(env$resp[[1]])]]
+    }
+    TRUE
+  }), mask)
+}
+
+get_stream_pattern <- function(service) {
+  switch(service,
+    "openai" = {
+      pattern <- '\\{"id":.*?\\}\\]\\}'
+      pluck <- c("choices", "delta", "content")
+    },
+    "anthropic" = {
+      pattern <- "\\{\"type\":\"completion\",.*\"log_id\":\"compl_[^\"]*\"\\}"
+      pluck <- "completion"
+    },
+    "perplexity" = {
+      pattern <- '\\{"id".*?\\}\\}\\]\\}'
+      pluck <- c("choices", "delta", "content")
+    },
+    "cohere" = {
+      pattern <-
+        '\\{"is_finished":false,"event_type":"text-generation","text":".*"\\}'
+      pluck <- "text"
+    },
+    "ollama" = {
+      pattern <- '\\{"model":.*"done":false\\}'
+      pluck <- "response"
+    }
+  )
+  list(pattern = pattern, pluck = pluck)
+}
